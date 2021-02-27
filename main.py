b83068dce23f3e244dc5d168bdac41245d1c0cdc
jushutch <jushutch@umich.edu>
2021-02-27 16:59:29 -0500

    Improve style and minor improvements
    Separated logic into functions, use os to build file paths, other minor changes.

--- main.py
+++ main.py
@@ -12,23 +12,31 @@ import string
 import re
 import getopt
 
+GET_IMAGE_OPTIONS = [
+    {'tag': 'img',
+     'attr': 'src',
+     'attr_filters': {}
+     },
+    {'tag': 'link',
+     'attr': 'href',
+     'attr_filters': {"type": "image/png"}
+     },
+]
+
 
 def get_images(url: urllib.parse.ParseResult, directory: str, absolute_image_url: bool) -> int:
     html_page = urllib.request.urlopen(url.geturl())
     soup = BeautifulSoup(html_page, features="html.parser")
-    # TODO make sure path building works for all os
-    for image_data, file_name in get_images_from_soup_by_type(soup, 'img', 'src', absolute_image_url):
-        with open(f"{directory}/{file_name}", 'wb') as file:
-            shutil.copyfileobj(image_data, file)
-    for image_data, file_name in get_images_from_soup_by_type(soup, 'link', 'href', absolute_image_url, attr_filters={"type": "image/png"}):
-        with open(f"{directory}/{file_name}", 'wb') as file:
-            shutil.copyfileobj(image_data, file)
+    for options in GET_IMAGE_OPTIONS:
+        for image_data, file_name in get_images_from_soup_by_type(soup, options, absolute_image_url, url):
+            with open(os.path.join(directory, file_name), 'wb') as file:
+                shutil.copyfileobj(image_data, file)
     return len(os.listdir(directory))
 
 
-def get_images_from_soup_by_type(soup: BeautifulSoup, tag: str, attr: str, absolute_image_url: bool, attr_filters: dict=None):
-    for image in soup.find_all(tag, recursive=True, attrs=attr_filters):
-        image_path = image.get(attr)
+def get_images_from_soup_by_type(soup: BeautifulSoup, options: dict, absolute_image_url: bool, url: urllib.parse.ParseResult):
+    for image in soup.find_all(options['tag'], recursive=True, attrs=options['attr_filters']):
+        image_path = image.get(options['attr'])
         if absolute_image_url:
             image_url = image_path
         else:
@@ -40,13 +48,12 @@ def get_images_from_soup_by_type(soup: BeautifulSoup, tag: str, attr: str, absol
 
 def extract_hidden_files(directory: str):
     for filename in os.listdir(directory):
-        # TODO faster alternative? extract to same folder?
-        binwalk.scan(f"{directory}/{filename}", quiet=True, signature=True, rm=True, extract=True,
+        binwalk.scan(os.path.join(directory, filename), quiet=True, signature=True, rm=True, extract=True,
                      directory=directory)
 
 
 def strings(filename, min=4):
-    with open(filename, errors="ignore") as f:  # Python 3.x
+    with open(filename, errors="ignore") as f:
         result = ""
         for c in f.read():
             if c in string.printable:
@@ -55,7 +62,7 @@ def strings(filename, min=4):
             if len(result) >= min:
                 yield result
             result = ""
-        if len(result) >= min:  # catch result at EOF
+        if len(result) >= min:
             yield result
 
 
@@ -69,7 +76,11 @@ def search_for_flags(directory: str, flag_prefix: str) -> list:
                 hidden = False
                 if "extracted" in currentpath:
                     hidden = True
-                flags_found.append({"flag": flag.pop(), "filename": file, "hidden": hidden})
+                flags_found.append({
+                    "flag": flag.pop(),
+                    "filename": file,
+                    "hidden": hidden
+                })
     return flags_found
 
 
@@ -79,7 +90,6 @@ def print_results(flags: list, number_of_images: int):
     print_flags(flags)
 
 
-# TODO add more descriptive output info: num_flags, file found, time to execute, number of files scanned
 def print_flags(flags: list):
     print(f"{'Filename' : <20}\t{'Hidden' : <10}\t{'Flag' : <30}")
     for flag in flags:
@@ -90,10 +100,7 @@ def extract_flag_from_string(string: str, flag_prefix: str) -> str:
     return re.findall(rf"{re.escape(flag_prefix)}{{.*}}", string).pop()
 
 
-if __name__ == '__main__':
-    if len(sys.argv) < 3:
-        print(f"usage: {sys.argv[0]} <options> <url> <flag prefix>", file=sys.stderr)
-        sys.exit(-1)
+def get_config_from_command_line(args: list) -> dict:
     try:
         options, remaining_arguments = getopt.getopt(sys.argv[1:], 'ad:', ['absolute-image-url', 'directory='])
     except getopt.GetoptError:
@@ -107,18 +114,28 @@ if __name__ == '__main__':
             absolute_image_url = True
         elif option in '-d' or option in '--directory':
             directory = argument
-
     url = urllib.parse.urlparse(remaining_arguments[0])
     flag_prefix = remaining_arguments[1]
+    return {'directory': directory, 'url': url, 'absolute_image_url': absolute_image_url, 'flag_prefix': flag_prefix}
 
+
+def make_temp_directory(directory: str) -> None:
     try:
         os.mkdir(directory)
     except OSError as error:
         print(f"There was a problem creating the directory {directory}:\n{error}", file=sys.stderr)
         sys.exit(-1)
 
-    number_of_images = get_images(url, directory, absolute_image_url)
-    extract_hidden_files(directory)
-    flags = search_for_flags(directory, flag_prefix)
+
+if __name__ == '__main__':
+    if len(sys.argv) < 3:
+        print(f"usage: {sys.argv[0]} <options> <url> <flag prefix>", file=sys.stderr)
+        sys.exit(-1)
+
+    config = get_config_from_command_line(sys.argv)
+    make_temp_directory(config['directory'])
+    number_of_images = get_images(config['url'], config['directory'], config['absolute_image_url'])
+    extract_hidden_files(config['directory'])
+    flags = search_for_flags(config['directory'], config['flag_prefix'])
     print_results(flags, number_of_images)
-    shutil.rmtree(directory)
+    shutil.rmtree(config['directory'])
